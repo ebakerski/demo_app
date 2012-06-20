53cb90e3835b3f4456a037a2f813391dda299adc
Eric Baker <ericbaker77@hotmail.com>
2012-06-20 13:27:17 -0600

    Automated Task Extract

--- config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-DemoApp::Application.config.secret_token = '9507c08401d3aeab28232ecb7844d81da7fa881f7addd1c0032e9c6e2a5eabd0c182ae680f02031f5ea88799d3d408f61701b1709c3c7e780577fb7a83b3e64e'
